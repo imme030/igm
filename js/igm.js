aa5af333c8798cbc83459f660098b95645066ef6
Eva Gothein <mail@gothein-design.de>
2014-11-20 20:17:24 +0100

    news modul

--- js/igm.js
+++ js/igm.js
@@ -10,4 +10,17 @@
             $(this).parent().siblings().removeClass('open');
             $(this).parent().toggleClass('open');
         });
+   $(function(){
+    
+    // Enabling Popover Example 1 - HTML (content and title from html tags of element)
+    $("[data-toggle=popover]").popover();
+    $("#popover-share").popover({
+        html : true, 
+        content: function() {
+          return $('#popover-share-content').html();
+        }
+    });
+
+});
+
   });
