f693e725da60977562ded3f66d036d62bf88a6c7
Eva Gothein <mail@gothein-design.de>
2014-11-25 20:41:02 +0100

    Accordion CSS angepasst

--- js/igm.js
+++ js/igm.js
@@ -10,9 +10,22 @@
             $(this).parent().siblings().removeClass('open');
             $(this).parent().toggleClass('open');
         });
-   $(function(){
-    
-    // Enabling Popover Example 1 - HTML (content and title from html tags of element)
+    // Accordion aktive Klasse
+
+   $('#accordion').collapse({
+  toggle: true
+
+})
+     $(".panel-heading h4").click(function(e) {
+        if( $(this).parent().parent().parent().hasClass("active") ) {
+            $(this).parent().parent().parent().removeClass("active")
+        } else {
+            $(this).parent().parent().parent().siblings().removeClass("active") 
+            $(this).parent().parent().parent().addClass("active");
+        }
+  });
+   $(function(){    
+    //  Popover- HTML (content and title from html tags of element)
     $("[data-toggle=popover]").popover();
     $("#popover-share").popover({
         html : true, 
