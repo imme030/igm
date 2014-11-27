0251d436b71600a4892b8852094972ac1ab36d86
Eva Gothein <mail@gothein-design.de>
2014-11-27 17:56:55 +0100

    header 3

--- js/igm.js
+++ js/igm.js
@@ -11,15 +11,17 @@
             $(this).parent().toggleClass('open');
         });
     // Accordion aktive Klasse
-
-    $(".panel-heading h4").click(function(e) {
+ $(".panel-heading h4").click(function() {
+       $(this).parent().parent().parent().toggleClass('active');
+    });
+    /* $(".panel-heading h4").click(function(e) {
         if( $(this).parent().parent().parent().hasClass("active") ) {
             $(this).parent().parent().parent().removeClass("active")
         } else {
             $(this).parent().parent().parent().siblings().removeClass("active") 
             $(this).parent().parent().parent().addClass("active");
         }
-  });
+  });*/
    $(function(){    
     //  Popover- HTML (content and title from html tags of element)
     $("[data-toggle=popover]").popover();
